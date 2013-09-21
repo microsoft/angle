1e232d7c7eaafc73b8af6bce30bd62d443364157
steven-chith <steven.chith@gmail.com>
2013-09-20 21:30:56 -0700

    Modified precompiler to take multiple shader files

--- src/glslPrecompiler/main.cpp
+++ src/glslPrecompiler/main.cpp
@@ -7,8 +7,11 @@
 #include "../samples/gles2_book/Common/esUtil.h"
 #include <stdio.h>
 #include <assert.h>
+#include <vector>
+#include <string>
 
 static void usage();
+static GLuint createShader(GLenum shaderType, const char **source, unsigned sourceCount);
 
 int main(int argc, char* argv[])
 {
@@ -22,9 +25,13 @@ int main(int argc, char* argv[])
     bool outputToHeaderFile = false;
     char outputFile[1024] = "shader.file";
     char variableName[1024];
+    std::vector<std::string> vertexShaders;
+    std::vector<std::string> fragmentShaders;
 
     if (argc < 3)
         usageFail = TRUE;
+    
+    GLuint program = glCreateProgram();
 
     argc--;
     argv++;
@@ -36,49 +43,39 @@ int main(int argc, char* argv[])
                     strcpy(outputFile, argv[0] + 3);
                 }
                 break;
-            case 'v':
+            case 'a':
                 if (argv[0][2] == '=') {
                     strcpy(variableName, argv[0] + 3);
                     outputToHeaderFile = true;
                 }
                 break;
+            case 'v':
+            case 'f':
+                {
+                    if(argv[0][2] != '=')
+                        break;
+
+                    FILE *fp = fopen(argv[0] + 3, "r");
+                    if(!fp)
+                    {
+                        printf("Can't open %s, aborting\n", argv[0]);
+                        return 0;
+                    }
+                    fseek(fp, 0, SEEK_END);
+                    int fileSize = ftell(fp);
+                    rewind(fp);
+                    char *sourceBuffer = new char[fileSize + 1];
+                    fileSize = fread(sourceBuffer, 1, fileSize, fp);
+                    fclose(fp);
+                    sourceBuffer[fileSize] = 0;
+                    if(argv[0][1] == 'v')
+                        vertexShaders.push_back(sourceBuffer);
+                    else
+                        fragmentShaders.push_back(sourceBuffer);
+                    delete [] sourceBuffer;
+                }
+                break;
             default: usageFail = true;
-            }
-        } else {
-            GLuint shader;
-            if(strstr(argv[0], ".vert"))
-                shader = vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            else
-                shader = fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-            FILE *fp = fopen(argv[0], "r");
-            if(!fp)
-            {
-                printf("Can't open %s, aborting\n", argv[0]);
-                return 0;
-            }
-            fseek(fp, 0, SEEK_END);
-            int fileSize = ftell(fp);
-            rewind(fp);
-            char *sourceBuffer = new char[fileSize + 1];
-            fileSize = fread(sourceBuffer, 1, fileSize, fp);
-            fclose(fp);
-            sourceBuffer[fileSize] = 0;
-            glShaderSource(shader, 1, (const char**)&sourceBuffer, NULL);
-            delete [] sourceBuffer;
-            glCompileShader(shader);
-            int compileStatus;
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
-
-            if(compileStatus != GL_TRUE)
-            {
-                glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &compileStatus);
-                sourceBuffer = new char[compileStatus];
-                glGetShaderInfoLog(shader, compileStatus, NULL, sourceBuffer);
-                printf("Failed to compile %s due to:\n%s\n", argv[0], sourceBuffer);
-                delete [] sourceBuffer;
-                glDeleteShader(shader);
-                return 0;
             }
         }
     }
@@ -89,9 +86,22 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    GLuint program = glCreateProgram();
+    std::vector<const char*> vertexSources;
+    std::vector<const char*> fragmentSources;
+    for(unsigned i = 0; i < vertexShaders.size(); ++i)
+        vertexSources.push_back(vertexShaders[i].c_str());
+    for(unsigned i = 0; i < fragmentShaders.size(); ++i)
+        fragmentSources.push_back(fragmentShaders[i].c_str());
+
+    vertexShader = createShader(GL_VERTEX_SHADER, &vertexSources[0], vertexSources.size());
+    if(!vertexShader)
+        return 0;
+    fragmentShader = createShader(GL_FRAGMENT_SHADER, &fragmentSources[0], vertexSources.size());
+    if(!fragmentShader)
+        return 0;
     glAttachShader(program, vertexShader);
     glAttachShader(program, fragmentShader);
+
     glLinkProgram(program);
     int linkStatus;
     glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
@@ -147,8 +157,30 @@ int main(int argc, char* argv[])
 //
 void usage()
 {
-    printf("Usage: glslPrecompiler [-o=[file] -v=[variable]] file1 file2\n"
+    printf("Usage: glslPrecompiler [-o=[file] -a=[variable]] -v=vertex_shader_1 ... -v=vertex_shader_1 -f=fragment_shader_1 ... -f=fragment_shader_n\n"
         "Where: filename : filename ending in .frag or .vert\n"
         "       -o=[file] : output file\n"
         "       -v=[variable] : output to char array named [variable] in header file, omit for binary file\n");
+}
+
+GLuint createShader(GLenum shaderType, const char **source, unsigned sourceCount)
+{
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, sourceCount, source, NULL);
+    glCompileShader(shader);
+    int compileStatus;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
+
+    if(compileStatus != GL_TRUE)
+    {
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &compileStatus);
+        char *sourceBuffer = new char[compileStatus];
+        char *shaderTypeName = (shaderType == GL_VERTEX_SHADER) ? "vertex shader(s)" : "fragment shader(s)";
+        glGetShaderInfoLog(shader, compileStatus, NULL, sourceBuffer);
+        printf("Failed to compile %s due to:\n%s\n", shaderTypeName, sourceBuffer);
+        delete [] sourceBuffer;
+        glDeleteShader(shader);
+        return 0;
+    }
+    return shader;
 }
