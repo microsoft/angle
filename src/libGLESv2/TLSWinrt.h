cc3b096ca47eecb084f3a479515a84e06862f33f
Dale Stammen <dalestam@microsoft.com>
2013-11-20 17:38:06 -0800

    winrt thread local storage implementation

---
+++ src/libGLESv2/TLSWinrt.h
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2002-2010 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// TLSWinrt.h: Thread local storage implementation for WinRT and WP8.
+
+#ifndef TLS_WINRT_H_
+#define TLS_WINRT_H
+
+#include "libGLESv2/main.h"
+
+#define TLS_OUT_OF_INDEXES -1
+
+void* TlsGetValue(DWORD index);
+void * LocalAlloc(UINT uFlags, size_t size);
+void LocalFree(HLOCAL index);
+DWORD TlsAlloc();
+void TlsSetValue(DWORD currentTLS, gl::Current* current);
+void TlsFree(DWORD index);
+
+#endif // TLS_WINRT_H_
