2c3cbd75d08e7b34dc29c04b9252a5ea979aa93e
Dale Stammen <dalestam@microsoft.com>
2014-05-22 10:36:12 -0700

    added buildall script

---
+++ nuget/buildall.ps1
@@ -0,0 +1,6 @@
+echo "building WinRT Angle..."
+Start-Process .\winrt_build.bat -wait
+Write-NuGetPackage .\angle_winrt.autopkg
+echo "building WP8 Angle..."
+Start-Process .\wp8_build.bat -wait
+Write-NuGetPackage .\angle_wp8.autopkg
