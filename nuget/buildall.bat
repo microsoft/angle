8a6cd6f60d69ca0138fefb752a97db08d6593abe
Austin Kinross <aukinros@microsoft.com>
2015-03-15 16:28:31 -0700

    Delete winrt branch. For ANGLE on Windows 8.0, visit www.github.com/MSOpenTech/angle-win8.0

--- nuget/buildall.bat
+++
@@ -1,1 +0,0 @@
-powershell.exe -ExecutionPolicy Bypass -Command ".\buildall.ps1"
--- nuget/buildall.ps1
+++
@@ -1,8 +0,0 @@
-echo "building Angle projects and nuget packages..."
-rm *.nupkg
-echo "building WinRT Angle..."
-Start-Process .\winrt_build.bat -wait
-Write-NuGetPackage .\angle_winrt.autopkg
-echo "building WP8 Angle..."
-Start-Process .\wp8_build.bat -wait
-Write-NuGetPackage .\angle_wp8.autopkg
