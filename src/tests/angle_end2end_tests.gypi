beaudie/vesc

Implement EXT_discard_framebuffer in D3D11 renderer
Change-Id: I52bcf0cfb1aa123e085a35730fdefb006b617c3c

src/tests/angle_end2end_tests.gypi
@@ -23,6 +23,7 @@
             '<(angle_path)/src/tests/end2end_tests/CompressedTextureTest.cpp',
             '<(angle_path)/src/tests/end2end_tests/CubeMapTextureTest.cpp',
             '<(angle_path)/src/tests/end2end_tests/DepthStencilFormatsTest.cpp',
+            '<(angle_path)/src/tests/end2end_tests/DiscardFramebufferEXTTest.cpp',
             '<(angle_path)/src/tests/end2end_tests/DrawBuffersTest.cpp',
             '<(angle_path)/src/tests/end2end_tests/FenceSyncTests.cpp',
             '<(angle_path)/src/tests/end2end_tests/FramebufferFormatsTest.cpp',

microsoft/angle

src/tests/angle_end2end_tests.gypi
@@ -20,6 +20,7 @@
             '<(angle_path)/src/tests/gl_tests/ClearTest.cpp',
             '<(angle_path)/src/tests/gl_tests/CompressedTextureTest.cpp',
             '<(angle_path)/src/tests/gl_tests/CubeMapTextureTest.cpp',
+            '<(angle_path)/src/tests/gl_tests/DiscardFramebufferEXTTest.cpp',
             '<(angle_path)/src/tests/gl_tests/DepthStencilFormatsTest.cpp',
             '<(angle_path)/src/tests/gl_tests/DrawBuffersTest.cpp',
             '<(angle_path)/src/tests/gl_tests/FenceSyncTests.cpp',